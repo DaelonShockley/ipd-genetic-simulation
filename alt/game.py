881ac6e5008cc6e4762ba6b5f22d74e0e2f662bf
Daelon Shockley <dtsdg4@umsystem.edu>
2025-02-01 08:34:31 -0600

    alt player fixes and game implementation

---
+++ alt/game.py
@@ -0,0 +1,59 @@
+from player import Player
+from itertools import combinations
+
+class Game():
+    @staticmethod
+    def run_round(players: list[Player], rounds_per_game, games_per_match, score_both_coop, score_both_def, score_player_def, score_opp_def):
+        for player1, player2 in combinations(players, 2):  # Iterate through all unique pairs of players
+            for _ in range(games_per_match):
+                Game.run_single_game(player1, player2, rounds_per_game, score_both_coop, score_both_def, score_player_def, score_opp_def)
+
+    @staticmethod
+    def run_single_game(player1, player2, rounds_per_game, score_both_coop, score_both_def, score_player_def, score_opp_def):
+        history_play_1 = "" #decisions of player 1
+        history_play_2 = "" #decisions of player 2
+        player1_score = 0
+        player2_score = 0
+
+        rounds_played = 0
+        while(rounds_played < rounds_per_game):
+            player1_decision = player1.get_decision(history_play_1, history_play_2)
+            player2_decision = player2.get_decision(history_play_2, history_play_1)
+
+            #remember that a decision of True means the player is defecting, similarly a "1" in the history represents a defection
+            if(not player1_decision and not player2_decision): #both players cooperate
+                player1_score += score_both_coop
+                player2_score += score_both_coop
+                history_play_1 += "0"
+                history_play_2 += "0"
+            elif(player1_decision and not player2_decision): #player1 defects, player2 cooperates
+                player1_score += score_player_def
+                player2_score += score_opp_def
+                history_play_1 += "1"
+                history_play_2 += "0"
+            elif(not player1_decision and player2_decision): #player1 cooperates, player2 defects
+                player1_score += score_opp_def
+                player2_score += score_player_def
+                history_play_1 += "0"
+                history_play_2 += "1"
+            else: #both players defect
+                player1_score += score_both_def
+                player2_score += score_both_def
+                history_play_1 += "1"
+                history_play_2 += "1"
+
+            rounds_played += 1
+
+        player1.total_score += player1_score
+        player2.total_score += player2_score
+
+        if(player1_score > player2_score):
+            player1.wins += 1
+            player2.losses += 1
+        elif(player1_score == player2_score):
+            player1.draws += 1
+            player2.draws += 1
+        else:
+            player2.wins += 1
+            player1.losses += 1
+
--- alt/player.py
+++ alt/player.py
@@ -6,24 +6,39 @@ class Player():
         self.opp_history_weight = np.random.uniform(-table_init_magnitude, table_init_magnitude, (rounds_per_game, 2))
         self.self_history_weight = np.random.uniform(-table_init_magnitude, table_init_magnitude, (rounds_per_game, 2))
 
+        self.total_score = 0
+        self.wins = 0
+        self.losses = 0
+        self.draws = 0
+
     def get_decision(self, self_history, opp_history):
         '''
-        get player decision, result of true means player will cooperate
+        get player decision, result of true means player will defect
         '''
         probabilities = []
 
+        if self_history == "":
+            probabilities.append(self.opp_history_weight[0][0])
+            probabilities.append(self.self_history_weight[i][int(self_history[-i])])
+            odds = np.mean(probabilities)
+            if random.random() <= odds:
+                return False #cooperate
+            else:
+                return True #defect 
+
+
         for i in range(len(opp_history) + 1):
-            probabilities.append(self.opp_history_weight[i][int(opp_history[-i])])
+            probabilities.append(self.opp_history_weight[i-1][int(opp_history[-i])])
 
         for i in range(len(self_history) + 1):
-            probabilities.append(self.self_history_weight[i][int(self_history[-i])])
+            probabilities.append(self.self_history_weight[i-1][int(self_history[-i])])
 
         odds = np.mean(probabilities)
 
         if random.random() <= odds:
-            return True
+            return False #cooperate
         else:
-            return False
+            return True #defect 
 
     
         
